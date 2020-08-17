161081bc1567b0898eb2e30108d46997c0e88b11
hatz7505 <harryteplow@hotmail.com>
2020-08-17 12:02:34 -0700

    connect page now mobile friendly

--- src/Connect.js
+++ src/Connect.js
@@ -10,7 +10,7 @@ import SiteNavbar from "./SiteNavbar";
 
 function AboutMe() {
   return (
-    <div>
+    <div className="connect-container">
       <SVG
         numOfCircles={500}
         distanceBetween={17}
@@ -26,32 +26,27 @@ function AboutMe() {
           <CardTitle>
             <h1 className="header">Connect with me!</h1>
           </CardTitle>
-          <CardText>
+          <CardText className="contact">
             I always love connecting with new people, feel free to shoot me an
             email or find me on LinkedIn!
-            <div className="contact">
-              <a className="email" href="mailto:harryteplow@hotmail.com">
-                <div className="icon-div">
-                  <FontAwesomeIcon icon={faEnvelope} />{" "}
-                </div>
-                Email me{" "}
-              </a>{" "}
-              <br></br>
-              <a
-                className="linkedinLink"
-                href="https://www.linkedin.com/in/harry-teplow-4a3b341b0/"
-                target="_blank"
-              >
-                <div className="icon-div">
-                  <FontAwesomeIcon icon={faLinkedinIn} />
-                </div>{" "}
-                Catch me on LinkedIn
-              </a>
-            </div>
+            <br></br>
+            <a className="email" href="mailto:harryteplow@gmail.com">
+              <FontAwesomeIcon className="icon-div" icon={faEnvelope} /> Email
+              me{" "}
+            </a>{" "}
+            <br></br>
+            <a
+              className="linkedinLink"
+              href="https://www.linkedin.com/in/harry-teplow-4a3b341b0/"
+              target="_blank"
+            >
+              <FontAwesomeIcon className="icon-div" icon={faLinkedinIn} /> Catch
+              me on LinkedIn
+            </a>
           </CardText>
         </CardBody>
       </Card>
-      <SiteNavbar stylingClass="navbar-connect"/>
+      <SiteNavbar stylingClass="navbar-connect" />
     </div>
   );
 }
