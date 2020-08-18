a503c90c2dbccf9adc2265e656ef888d982d2391
hatz7505 <harryteplow@hotmail.com>
2020-08-18 15:43:53 -0700

    connect spacing and styling updated

--- src/Connect.js
+++ src/Connect.js
@@ -27,14 +27,14 @@ function AboutMe() {
             <h1 className="header">Connect with me!</h1>
           </CardTitle>
           <CardText className="contact">
-            I always love connecting with new people, feel free to shoot me an
-            email or find me on LinkedIn!
-            <br></br>
+            <span className="connect-text">
+              I always love connecting with new people, feel free to shoot me an
+              email or find me on LinkedIn!
+            </span>
             <a className="email" href="mailto:harryteplow@gmail.com">
               <FontAwesomeIcon className="icon-div" icon={faEnvelope} /> Email
               me{" "}
             </a>{" "}
-            <br></br>
             <a
               className="linkedinLink"
               href="https://www.linkedin.com/in/harry-teplow-4a3b341b0/"
