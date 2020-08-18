8d60a72464c445be9e5cfd767e28bade7a32e59b
hatz7505 <harryteplow@hotmail.com>
2020-08-17 17:37:10 -0700

    added navbar to projects component

--- src/Projects.js
+++ src/Projects.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SVG from "./SVG";
+import SiteNavbar from "./SiteNavbar";
 import "./Projects.css";
 import { Card, CardBody, CardTitle, CardText, Row, Col } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -13,140 +14,141 @@ import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 function Projects() {
   return (
     <div className="projects-container">
-      {/* <SVG
+      <SVG
         numOfCircles={500}
         distanceBetween={17}
         charge={60}
         withForce={false}
-      /> */}
+      />
       <div className="projects-text-container">
         <h1 className="projects-header">Some things I've built</h1>
-        <Row>
-          <Col xs="6">
-            <Card className="project" style={{ backgroundColor: "white" }}>
-              <CardBody>
-                <CardTitle>
-                  <h2 className="project-title">
-                    Warbler <FontAwesomeIcon icon={faTwitter} />
-                  </h2>
-                </CardTitle>
-                <CardText className="project-text">
-                  A Twitter clone application built with Python, Flask, and
-                  PostgreSQL.
-                  <div className="warbly-heroku-link">
-                    Deployed on{" "}
-                    <a
-                      className="project-link"
-                      href="https://warbler-star.herokuapp.com/"
-                      target="_blank"
-                    >
-                      Heroku
-                    </a>
-                  </div>
-                  <div>
-                    View the source code on{" "}
-                    <a
-                      className="project-link"
-                      href="https://github.com/hatz7505/warbler"
-                      target="_blank"
-                    >
-                      Github
-                    </a>
-                  </div>
-                </CardText>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col xs="6">
-            <Card className="project" style={{ backgroundColor: "white" }}>
-              <CardBody>
-                <CardTitle>
-                  <h2 className="project-title">
-                    Microblog <FontAwesomeIcon icon={faLaptop} />
-                  </h2>
-                </CardTitle>
-                <CardText className="project-text">
-                  A blogging app built using React and Redux. Incorporates basic
-                  blogging functionalities, including adding, editing, and
-                  deleting blog posts and blog post comments, as well as up- and
-                  down-voting.
-                  <div className="microblog-github-link">
-                    View the source code on{" "}
-                    <a
-                      className="project-link"
-                      href="https://github.com/vanvemden/react-redux-microblog-frontend"
-                      target="_blank"
-                    >
-                      Github
-                    </a>
-                  </div>
-                </CardText>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs="6">
-            <Card className="project" style={{ backgroundColor: "white" }}>
-              <CardBody>
-                <CardTitle>
-                  <h2 className="project-title">
-                    Job.ly <FontAwesomeIcon icon={faSearchDollar} />
-                  </h2>
-                </CardTitle>
-                <CardText className="project-text">
-                  A job posting and application site built using React on the
-                  frontend, Express and PostgreSQL on the backend.
-                  <div className="jobly-github-link">
-                    View the source code on{" "}
-                    <a
-                      className="project-link"
-                      href="https://github.com/hatz7505/jobly"
-                      target="_blank"
-                    >
-                      Github
-                    </a>
-                  </div>
-                </CardText>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col xs="6">
-            <Card className="project" style={{ backgroundColor: "white" }}>
-              <CardBody>
-                <CardTitle>
-                  <h2 className="project-title">
-                    Match a Kween <FontAwesomeIcon icon={faCrown} />
-                  </h2>
-                </CardTitle>
-                <CardText className="project-text">
-                  A card matching memory game. Built as a single page web
-                  application using JavaScript and JQuery.
-                  <div className="warbly-heroku-link">
-                    Deployed on{" "}
-                    <a
-                      className="project-link"
-                      href="https://hatz7505.github.io/memory-game/"
-                      target="_blank"
-                    >
-                      Github
-                    </a>
-                  </div>
-                  <div>
-                    View the source code on{" "}
-                    <a
-                      className="project-link"
-                      href="https://github.com/hatz7505/memory-game"
-                      target="_blank"
-                    >
-                      Github
-                    </a>
-                  </div>
-                </CardText>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
+          <Row>
+            <Col xs="6">
+              <Card className="project" style={{ backgroundColor: "white" }}>
+                <CardBody>
+                  <CardTitle>
+                    <h2 className="project-title">
+                      Warbler <FontAwesomeIcon icon={faTwitter} />
+                    </h2>
+                  </CardTitle>
+                  <CardText className="project-text">
+                    A Twitter clone application built with Python, Flask, and
+                    PostgreSQL.
+                    <div className="warbly-heroku-link">
+                      Deployed on{" "}
+                      <a
+                        className="project-link"
+                        href="https://warbler-star.herokuapp.com/"
+                        target="_blank"
+                      >
+                        Heroku
+                      </a>
+                    </div>
+                    <div>
+                      View the source code on{" "}
+                      <a
+                        className="project-link"
+                        href="https://github.com/hatz7505/warbler"
+                        target="_blank"
+                      >
+                        Github
+                      </a>
+                    </div>
+                  </CardText>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col xs="6">
+              <Card className="project" style={{ backgroundColor: "white" }}>
+                <CardBody>
+                  <CardTitle>
+                    <h2 className="project-title">
+                      Microblog <FontAwesomeIcon icon={faLaptop} />
+                    </h2>
+                  </CardTitle>
+                  <CardText className="project-text">
+                    A blogging app built using React and Redux. Incorporates
+                    basic blogging functionalities, including adding, editing,
+                    and deleting blog posts and blog post comments, as well as
+                    up- and down-voting.
+                    <div className="microblog-github-link">
+                      View the source code on{" "}
+                      <a
+                        className="project-link"
+                        href="https://github.com/vanvemden/react-redux-microblog-frontend"
+                        target="_blank"
+                      >
+                        Github
+                      </a>
+                    </div>
+                  </CardText>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs="6">
+              <Card className="project" style={{ backgroundColor: "white" }}>
+                <CardBody>
+                  <CardTitle>
+                    <h2 className="project-title">
+                      Job.ly <FontAwesomeIcon icon={faSearchDollar} />
+                    </h2>
+                  </CardTitle>
+                  <CardText className="project-text">
+                    A job posting and application site built using React on the
+                    frontend, Express and PostgreSQL on the backend.
+                    <div className="jobly-github-link">
+                      View the source code on{" "}
+                      <a
+                        className="project-link"
+                        href="https://github.com/hatz7505/jobly"
+                        target="_blank"
+                      >
+                        Github
+                      </a>
+                    </div>
+                  </CardText>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col xs="6">
+              <Card className="project" style={{ backgroundColor: "white" }}>
+                <CardBody>
+                  <CardTitle>
+                    <h2 className="project-title">
+                      Match a Kween <FontAwesomeIcon icon={faCrown} />
+                    </h2>
+                  </CardTitle>
+                  <CardText className="project-text">
+                    A card matching memory game. Built as a single page web
+                    application using JavaScript and JQuery.
+                    <div className="warbly-heroku-link">
+                      Deployed on{" "}
+                      <a
+                        className="project-link"
+                        href="https://hatz7505.github.io/memory-game/"
+                        target="_blank"
+                      >
+                        Github
+                      </a>
+                    </div>
+                    <div>
+                      View the source code on{" "}
+                      <a
+                        className="project-link"
+                        href="https://github.com/hatz7505/memory-game"
+                        target="_blank"
+                      >
+                        Github
+                      </a>
+                    </div>
+                  </CardText>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        <SiteNavbar />
       </div>
     </div>
   );
